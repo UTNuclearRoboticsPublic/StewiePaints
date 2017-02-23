ddf51346f06a8526c0233ea147da9384649bd597
AndyZelenak <andyz@utexas.edu>
2017-02-22 18:33:43 -0600

    Defined robot poses

--- include/stewie_paints/define_paints_strokes.h
+++ include/stewie_paints/define_paints_strokes.h
@@ -20,35 +20,35 @@ using namespace stewie_paints;
 // Define the names and locations of paints and water
 void define_paints_and_water()
 {
-	water.paint_pose.pose.position.x = 0.;
-	water.paint_pose.pose.position.y = 0.;
-	water.paint_pose.pose.position.z = 0.;
-	water.paint_pose.pose.orientation.x = 0.;
-	water.paint_pose.pose.orientation.y = 0.;
-	water.paint_pose.pose.orientation.z = 0.;
-	water.paint_pose.pose.orientation.w = 1.;
+	water.paint_pose.pose.position.x = 0.0627145;
+	water.paint_pose.pose.position.y = 0.230904;
+	water.paint_pose.pose.position.z = 0.312077;
+	water.paint_pose.pose.orientation.x = 0.902623;
+	water.paint_pose.pose.orientation.y = 0.0057038;
+	water.paint_pose.pose.orientation.z = 0.00547319;
+	water.paint_pose.pose.orientation.w = -0.430358;
 	water.paint_pose.header.stamp = ros::Time::now();
 	water.paint_pose.header.frame_id = "base_link";
 
-	black.paint_pose.pose.position.x = 0.;
-	black.paint_pose.pose.position.y = 0.;
-	black.paint_pose.pose.position.z = 0.;
-	black.paint_pose.pose.orientation.x = 0.;
-	black.paint_pose.pose.orientation.y = 0.;
-	black.paint_pose.pose.orientation.z = 0.;
-	black.paint_pose.pose.orientation.w = 1.;
-	black.paint_pose.header.stamp = ros::Time::now();
-	black.paint_pose.header.frame_id = "base_link";
-
-	red.paint_pose.pose.position.x = 0.;
-	red.paint_pose.pose.position.y = 0.;
-	red.paint_pose.pose.position.z = 0.;
-	red.paint_pose.pose.orientation.x = 0.;
-	red.paint_pose.pose.orientation.y = 0.;
-	red.paint_pose.pose.orientation.z = 0.;
-	red.paint_pose.pose.orientation.w = 1.;
+	red.paint_pose.pose.position.x = 0.170114;
+	red.paint_pose.pose.position.y = 0.189005;
+	red.paint_pose.pose.position.z = 0.522692;
+	red.paint_pose.pose.orientation.x = 0.918429;
+	red.paint_pose.pose.orientation.y = 0.0497895;
+	red.paint_pose.pose.orientation.z = -0.0240753;
+	red.paint_pose.pose.orientation.w = -0.3917;
 	red.paint_pose.header.stamp = ros::Time::now();
 	red.paint_pose.header.frame_id = "base_link";
+
+	black.paint_pose.pose.position.x = 0.168679;
+	black.paint_pose.pose.position.y = 0.219773;
+	black.paint_pose.pose.position.z = 0.546762;
+	black.paint_pose.pose.orientation.x = 0.855938;
+	black.paint_pose.pose.orientation.y = -0.00878253;
+	black.paint_pose.pose.orientation.z = 0.0157565;
+	black.paint_pose.pose.orientation.w = 0.516764;
+	black.paint_pose.header.stamp = ros::Time::now();
+	black.paint_pose.header.frame_id = "base_link";
 }
 
 // The different types of strokes
